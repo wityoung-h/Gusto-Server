55f3ecf6f44bc4ea1e996ae4b683e49a27b3fe58
zzo3ozz <wityoung.h@gmail.com>
2024-01-12 15:36:45 +0900

    first commit project structure

---
+++ gusto/src/main/java/com/umc/gusto/GustoApplication.java
@@ -0,0 +1,13 @@
+package com.umc.gusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GustoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GustoApplication.class, args);
+    }
+
+}
---
+++ gusto/src/test/java/com/umc/gusto/GustoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.gusto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GustoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
