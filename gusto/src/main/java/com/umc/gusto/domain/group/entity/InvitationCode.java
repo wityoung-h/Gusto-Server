66aaa05c6ca50aa284c6393b1b3fd1e80809c438
zzo3ozz <wityoung.h@gmail.com>
2024-01-18 06:38:27 +0900

    feat: InvitationCodeEntity 생성

---
+++ gusto/src/main/java/com/umc/gusto/domain/group/entity/InvitationCode.java
@@ -0,0 +1,32 @@
+package com.umc.gusto.domain.group.entity;
+
+import com.umc.gusto.global.common.BaseTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+public class InvitationCode extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long invitationCodeId;
+
+    @OneToOne
+    @JoinColumn(name = "groupId", nullable = false)
+    private Group group;
+
+    @Column(columnDefinition = "VARCHAR(6)")
+    private String code;
+}
