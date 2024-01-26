cef1c620949089ab4ed9edee847cc5910a979d2c
kchaeeun <oceanball38@naver.com>
2024-01-27 01:55:26 +0900

    refactor : api 성공 시 return 값 수정

--- gusto/src/main/java/com/umc/gusto/apiPayload/code/BaseErrorCode.java
+++
@@ -1,8 +0,0 @@
-package com.umc.gusto.apiPayload.code;
-
-public interface BaseErrorCode {
-
-    public ErrorReasonDTO getReason();
-
-    public ErrorReasonDTO getReasonHttpStatus();
-}
--- gusto/src/main/java/com/umc/gusto/domain/myCategory/controller/MyCategoryController.java
+++ gusto/src/main/java/com/umc/gusto/domain/myCategory/controller/MyCategoryController.java
@@ -1,12 +1,12 @@
 package com.umc.gusto.domain.myCategory.controller;
 
-import com.umc.gusto.apiPayload.ApiResponse;
 import com.umc.gusto.domain.myCategory.entity.MyCategory;
 import com.umc.gusto.domain.myCategory.model.request.MyCategoryRequest;
 import com.umc.gusto.domain.myCategory.model.response.MyCategoryResponse;
 import com.umc.gusto.domain.myCategory.service.MyCategoryCommandService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,50 +18,50 @@ public class MyCategoryController {
     private final MyCategoryCommandService myCategoryCommandService;
 
     @GetMapping("/")
-    public ApiResponse<List<MyCategoryResponse.MyCategoryDTO>> allMyCategory(
+    public ResponseEntity<List<MyCategoryResponse.MyCategoryDTO>> allMyCategory(
             @RequestParam(name = "dong", required = false) String dong
     ) {
         try {
             List<MyCategoryResponse.MyCategoryDTO> myCategoryList = myCategoryCommandService.getAllMyCategory();
-            return ApiResponse.onSuccess(myCategoryList);
+            return ResponseEntity.ok(myCategoryList);
         } catch (Exception e) {
             // Handle the exception and return a failure response
-            return ApiResponse.onFailure("ERROR_CODE", "An error occurred", null);
+            throw new RuntimeException("MyCategory with the same name already exists and is in ACTIVE");
         }
     }
 
     @GetMapping("/{myCategoryId}/myStores")
-    public ApiResponse<List<MyCategoryResponse.MyStoreByMyCategoryDTO>> allMyStoreByMyCategory(
+    public ResponseEntity<List<MyCategoryResponse.MyStoreByMyCategoryDTO>> allMyStoreByMyCategory(
             @RequestParam(name = "dong", required = false) String dong,
             @PathVariable Long myCategoryId) {
         try {
             List<MyCategoryResponse.MyStoreByMyCategoryDTO> myStoreList = myCategoryCommandService.getAllMyStoreByMyCategory(myCategoryId);
-            return ApiResponse.onSuccess(myStoreList);
+            return ResponseEntity.ok(myStoreList);
         } catch (Exception e) {
             // Handle the exception and return a failure response
-            return ApiResponse.onFailure("ERROR_CODE", "An error occurred", null);
+            throw new RuntimeException("error");
         }
     }
 
     @PostMapping("/create")
-    public ApiResponse<String> createMyCategory(
+    public ResponseEntity<String> createMyCategory(
             @RequestBody @Valid MyCategoryRequest.createMyCategoryDTO createMyCategoryDTO
     ) {
         try {
             // 여기서 createMyCategoryDTO를 사용하여 새로운 MyCategory를 생성하는 로직을 구현합니다.
             myCategoryCommandService.createMyCategory(createMyCategoryDTO);
 
-            return ApiResponse.onSuccess("SUCCESS");
+            return ResponseEntity.ok("success");
         } catch (Exception e) {
             // 예외가 발생하면 처리하고 실패 응답을 반환합니다.
-            return ApiResponse.onFailure("ERROR_CODE", "An error occurred while creating MyCategory", null);
+            throw new RuntimeException("error");
         }
     }
 
 
 
     @PatchMapping("/{myCategoryId}/update")
-    public ApiResponse<MyCategory> modifyMyCategory(
+    public ResponseEntity<MyCategory> modifyMyCategory(
             @PathVariable Long myCategoryId,
             @RequestBody MyCategoryRequest.updateMyCategoryDTO request
     ) {
@@ -69,21 +69,21 @@ public class MyCategoryController {
             myCategoryCommandService.modifyMyCategory(myCategoryId, request);
         } catch (Exception e) {
             // 수정 중 에러가 발생한 경우, 실패 응답을 반환
-            return ApiResponse.onFailure("ERROR_CODE", "An error occurred while modifying the category", null);
+            throw new RuntimeException("error");
         }
 
         return null;
     }
 
     @PatchMapping("/{myCategoryId}/delete")
-    public ApiResponse<String> deleteMyCategory(@PathVariable Long myCategoryId, MyCategoryRequest.deleteMyCategoryDTO request) {
+    public ResponseEntity<String> deleteMyCategory(@PathVariable Long myCategoryId, MyCategoryRequest.deleteMyCategoryDTO request) {
         try {
             myCategoryCommandService.deleteMyCategory(myCategoryId, request);
 
-            return ApiResponse.onSuccess("Category deleted successfully");
+            return ResponseEntity.ok("Category deleted successfully");
         } catch (Exception e) {
             // 삭제 중 에러가 발생한 경우, 실패 응답을 반환
-            return ApiResponse.onFailure("ERROR_CODE", "An error occurred while deleting the category", null);
+            throw new RuntimeException("error");
         }
     }
 }
