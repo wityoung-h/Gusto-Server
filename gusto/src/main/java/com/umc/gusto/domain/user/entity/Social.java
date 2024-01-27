996b66178130365668121a66f5587a1994c5aaae
zzo3ozz <wityoung.h@gmail.com>
2024-01-28 03:32:22 +0900

    refactor: logic 변경에 따른 Social entity 수정
    가입용 token으로 사용할 uuid를 저장하며, social status를 복원함

--- gusto/src/main/java/com/umc/gusto/domain/user/entity/Social.java
+++ gusto/src/main/java/com/umc/gusto/domain/user/entity/Social.java
@@ -1,15 +1,18 @@
 package com.umc.gusto.domain.user.entity;
 
 import com.umc.gusto.global.common.BaseEntity;
+import com.umc.gusto.global.common.BaseTime;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.UUID;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Social extends BaseEntity {
+public class Social extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,11 +25,22 @@ public class Social extends BaseEntity {
     @Column(nullable = false)
     private String providerId;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 15)
+    private SocialStatus socialStatus = SocialStatus.WAITING_SIGN_UP;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId")
     private User user;
 
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID temporalToken;
+
     public enum SocialType {
         KAKAO, NAVER, GOOGLE
+    }
+
+    public enum SocialStatus {
+        CONNECTED, WAITING_SIGN_UP, DISCONNECTED
     }
 }
