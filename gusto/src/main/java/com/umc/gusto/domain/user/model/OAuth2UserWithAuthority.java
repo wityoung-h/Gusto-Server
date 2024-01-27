86e95835b942da97ad949fdc37d72d58e038dc71
zzo3ozz <wityoung.h@gmail.com>
2024-01-27 09:58:27 +0900

    feat: custom OAutn2User class 생성

---
+++ gusto/src/main/java/com/umc/gusto/domain/user/model/OAuth2UserWithAuthority.java
@@ -0,0 +1,34 @@
+package com.umc.gusto.domain.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class OAuth2UserWithAuthority implements OAuth2User {
+    private final OAuth2User delegate;
+    private final Collection<? extends GrantedAuthority> authorities;
+    private final OAuthAttributes oAuthAttributes;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return delegate.getAttributes();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getName() {
+        return delegate.getName();
+    }
+}
