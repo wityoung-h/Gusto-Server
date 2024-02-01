03a97d2895fc36e39c5569345c0e790e43eee725
hyesoo kim <khwater1017@naver.com>
2024-02-02 05:21:24 +0900

    feat: aws s3 이미지 저장

---
+++ gusto/src/main/java/com/umc/gusto/global/s3/S3Controller.java
@@ -0,0 +1,28 @@
+package com.umc.gusto.global.s3;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("image")
+@RequiredArgsConstructor
+public class S3Controller {
+
+    private final S3Service imageService;
+
+    @PostMapping()
+    public ResponseEntity<List<String>> uploadImages(
+            @RequestPart(value = "file", required = false) List<MultipartFile> images) {
+        List<String> fileUrls = imageService.uploadImages(images);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
