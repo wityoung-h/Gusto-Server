57c15b28309c0b0804218152ab7b5eb34c3c23bf
zzo3ozz <wityoung.h@gmail.com>
2024-02-02 20:21:34 +0900

    fix: record to class 전환에 따른 serializer error 수정

--- gusto/src/main/java/com/umc/gusto/domain/user/model/response/FirstLogInResponse.java
+++ gusto/src/main/java/com/umc/gusto/domain/user/model/response/FirstLogInResponse.java
@@ -1,13 +1,18 @@
 package com.umc.gusto.domain.user.model.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Builder
 @AllArgsConstructor
 public class FirstLogInResponse {
+    @JsonProperty
     private String nickname;
+    @JsonProperty
     private String profileImg;
+    @JsonProperty
     private String gender;
+    @JsonProperty
     private String age;
 }
