6de009cdf11f9a01f7d2f5e2dc99aa344d262eac
jeonghee-Han <134862850+Jeonghee-Han@users.noreply.github.com>
2024-02-05 21:26:08 +0900

    refactor: Http 상태 처리 수정

--- gusto/src/main/java/com/umc/gusto/domain/group/controller/GroupController.java
+++ gusto/src/main/java/com/umc/gusto/domain/group/controller/GroupController.java
@@ -1,17 +1,19 @@
 package com.umc.gusto.domain.group.controller;
 
-import com.umc.gusto.domain.group.model.request.GroupRequestDto;
-import com.umc.gusto.domain.group.model.response.GroupResponseDto;
+import com.umc.gusto.domain.group.model.request.PostGroupRequest;
+import com.umc.gusto.domain.group.model.request.UpdateGroupRequest;
+import com.umc.gusto.domain.group.model.response.GetGroupResponse;
+import com.umc.gusto.domain.group.model.response.PostGroupResponse;
+import com.umc.gusto.domain.group.model.response.UpdateGroupResponse;
 import com.umc.gusto.domain.group.service.GroupService;
 import com.umc.gusto.domain.user.entity.User;
 import com.umc.gusto.global.auth.model.AuthUser;
+import jdk.jfr.Event;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,9 +26,9 @@ public class GroupController {
      * [POST] /groups
      */
     @PostMapping
-    public ResponseEntity<GroupResponseDto.PostGroupResponseDto> createGroup(@AuthenticationPrincipal AuthUser authUser, @RequestBody GroupRequestDto.CreateGroupDTO createGroupDTO){
+    public ResponseEntity<?> createGroup(@AuthenticationPrincipal AuthUser authUser, @RequestBody PostGroupRequest postGroupRequest){
         User owner = authUser.getUser();
-        groupService.createGroup(owner, createGroupDTO);
+        groupService.createGroup(owner, postGroupRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
@@ -35,9 +37,9 @@ public class GroupController {
      * [GET] /groups/{groupId}
      */
     @GetMapping("/{groupId}")
-    public ResponseEntity<GroupResponseDto.GetGroupResponseDto> getGroup(@PathVariable Long groupId){
-        GroupResponseDto.GetGroupResponseDto getGroup = groupService.getGroup(groupId);
-        return ResponseEntity.ok().body(getGroup);
+    public ResponseEntity<GetGroupResponse> getGroup(@PathVariable Long groupId){
+        GetGroupResponse getGroup = groupService.getGroup(groupId);
+        return ResponseEntity.status(HttpStatus.OK).body(getGroup);
     }
 
     /**
@@ -45,10 +47,10 @@ public class GroupController {
      * [PATCH] /groups/{groupId}
      */
     @PatchMapping("/{groupId}")
-    public ResponseEntity<GroupResponseDto.UpdateGroupResponseDto> updateGroup(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long groupId, @RequestBody GroupRequestDto.UpdateGroupDTO updateGroupDTO){
+    public ResponseEntity<UpdateGroupResponse> updateGroup(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long groupId, @RequestBody UpdateGroupRequest updateGroupRequest){
         User owner = authUser.getUser();
-        GroupResponseDto.UpdateGroupResponseDto updatedGroup = groupService.updateGroup(owner, groupId, updateGroupDTO);
-        return ResponseEntity.ok().body(updatedGroup);
+        UpdateGroupResponse updatedGroup = groupService.updateGroup(owner, groupId, updateGroupRequest);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(updatedGroup);
     }
 
     /**
@@ -56,10 +58,10 @@ public class GroupController {
      * [PATCH] /groups/{groupId}/delete
      */
     @PatchMapping("/{groupId}/delete")
-    public ResponseEntity<String> deleteGroup(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long groupId){
+    public ResponseEntity<?> deleteGroup(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long groupId){
         User owner = authUser.getUser();
         groupService.deleteGroup(owner, groupId);
-        return ResponseEntity.ok("DELETE SUCCESS");
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
