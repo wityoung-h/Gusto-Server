3ec74be35551f53019f9830526164fb899a022e1
zzo3ozz <wityoung.h@gmail.com>
2024-02-08 04:39:14 +0900

    chore: 누락된 Getter Annotation 적용

--- gusto/src/main/java/com/umc/gusto/domain/user/model/response/FirstLogInResponse.java
+++ gusto/src/main/java/com/umc/gusto/domain/user/model/response/FirstLogInResponse.java
@@ -3,9 +3,11 @@ package com.umc.gusto.domain.user.model.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 @AllArgsConstructor
+@Getter
 public class FirstLogInResponse {
     @JsonProperty
     private String nickname;
