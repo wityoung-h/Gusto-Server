611b15bfa04ee3ca84aa44ae072e322887166e50
hyesoo kim <khwater1017@naver.com>
2024-02-09 04:56:32 +0900

    feat : JPQL로 리뷰에서 대표 상점 이미지 출력

--- gusto/src/main/java/com/umc/gusto/domain/review/repository/ReviewRepository.java
+++ gusto/src/main/java/com/umc/gusto/domain/review/repository/ReviewRepository.java
@@ -4,6 +4,7 @@ import com.umc.gusto.domain.review.entity.Review;
 import com.umc.gusto.domain.store.entity.Store;
 import com.umc.gusto.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,4 +13,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long > {
     Integer countByStoreAndUser(Store store, User user);
     boolean existsByReviewIdAndUser(Long reviewId, User user);
     Integer countByStoreAndUserNickname(Store store, String nickname);
+
+    @Query("SELECT r.img1 FROM Review r WHERE r.store.storeId = :storeId ORDER BY r.liked DESC")
+    Optional<String> findTopReviewImageByStoreId(Long storeId);
 }
