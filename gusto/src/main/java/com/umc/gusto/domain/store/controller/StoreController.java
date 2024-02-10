916692ebb71439bb39e506780b2d6e99de8c3411
kchaeeun <oceanball38@naver.com>
2024-02-11 02:25:05 +0900

    refactor : 가게 상세 조회 시 리뷰 날짜별 분류 처리

--- gusto/src/main/java/com/umc/gusto/domain/store/controller/StoreController.java
+++ gusto/src/main/java/com/umc/gusto/domain/store/controller/StoreController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -44,12 +45,13 @@ public class StoreController {
     public ResponseEntity<GetStoreDetailResponse> getStoreDetail(
             @AuthenticationPrincipal AuthUser authUser,
             @PathVariable Long storeId,
+            @RequestParam(name = "visitedAt", required = false) LocalDate visitedAt,
             @RequestParam(name = "reviewId", required = false) Long reviewId){
         User user = authUser.getUser();
         Pageable pageable = PageRequest.of(0, 3);
 
         // 상점 세부 정보 가져오기
-        GetStoreDetailResponse getStoreDetail = storeService.getStoreDetail(user, storeId, reviewId, pageable);
+        GetStoreDetailResponse getStoreDetail = storeService.getStoreDetail(user, storeId, visitedAt, reviewId, pageable);
         return ResponseEntity.status(HttpStatus.OK).body(getStoreDetail);
     }
 
