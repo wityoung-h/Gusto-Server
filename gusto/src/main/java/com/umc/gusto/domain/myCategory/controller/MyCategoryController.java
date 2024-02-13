2d8f7d98c77e3138cff91262cddf14ea253f950b
kchaeeun <oceanball38@naver.com>
2024-02-13 16:24:38 +0900

    refactor : 유효성 검사 & 데모데이 이후 내용 주석 처리

--- gusto/src/main/java/com/umc/gusto/domain/myCategory/controller/MyCategoryController.java
+++ gusto/src/main/java/com/umc/gusto/domain/myCategory/controller/MyCategoryController.java
@@ -20,6 +20,10 @@ import java.util.List;
 public class MyCategoryController {
     private final MyCategoryService myCategoryService;
 
+    /**
+     * 카테고리 전체 조회
+     * [GET] /myCategories/{nickname}
+     */
     @GetMapping("/{nickname}")
     public ResponseEntity<List<MyCategoryResponse>> allMyCategory(
             @AuthenticationPrincipal AuthUser authUser,
@@ -30,6 +34,10 @@ public class MyCategoryController {
         return ResponseEntity.status(HttpStatus.OK).body(myCategoryList);
     }
 
+    /**
+     * 위치 기반 내 카테고리 전체 조회
+     * [GET] /myCategories?townName={townName}
+     */
     @GetMapping
     public ResponseEntity<List<MyCategoryResponse>> allMyCategoryWithLocation(
             @AuthenticationPrincipal AuthUser authUser,
@@ -40,6 +48,10 @@ public class MyCategoryController {
             return ResponseEntity.status(HttpStatus.OK).body(myCategoryList);
     }
 
+    /**
+     * 카테고리 별 가게 목록 조회
+     * [GET] /myCategories/pins/{nickname}?myCategoryId={myCategoryId}
+     */
     @GetMapping("/pins/{nickname}")
     public ResponseEntity<List<PinByMyCategoryResponse>> allPinByMyCategory(
             @RequestParam(name = "myCategoryId") Long myCategoryId,
@@ -49,6 +61,10 @@ public class MyCategoryController {
             return ResponseEntity.status(HttpStatus.OK).body(myStoreList);
     }
 
+    /**
+     * 위치 기반 내 카테고리 별 가게 목록 조회
+     * [GET] /myCategories/pins?myCategoryId={mycategoryId}&townName={townName}
+     */
     @GetMapping("/pins")
     public ResponseEntity<List<PinByMyCategoryResponse>> allPinByCategoryWithLocation(
             @AuthenticationPrincipal AuthUser authUser,
@@ -60,7 +76,10 @@ public class MyCategoryController {
             return ResponseEntity.status(HttpStatus.OK).body(myCategoryList);
     }
 
-
+    /**
+     * 내 카테고리 생성
+     * [POST] /myCategories
+     */
     @PostMapping
     public ResponseEntity<?> createMyCategory(
             @AuthenticationPrincipal AuthUser authUser,
@@ -72,6 +91,10 @@ public class MyCategoryController {
             return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     * 내 카테고리 수정
+     * [PATCH] /myCategories/{myCategoryId}
+     */
     @PatchMapping("/{myCategoryId}")
     public ResponseEntity<?> modifyMyCategory(
             @AuthenticationPrincipal AuthUser authUser,
@@ -84,6 +107,10 @@ public class MyCategoryController {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
+    /**
+     * 내 카테고리 삭제
+     * [DELETE] /myCategories?myCategoryId={myCategoryId}&myCategoryId={myCategoryId}...
+     */
     @DeleteMapping
     public ResponseEntity<?> deleteMyCategory(
             @AuthenticationPrincipal AuthUser authUser,
