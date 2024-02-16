887084c3b241ceb1aaf0c2eb8fd726396004053b
kchaeeun <oceanball38@naver.com>
2024-02-16 16:26:05 +0900

    refactor : 위치 기반 API 분류 없이 통일 리펙토리

--- gusto/src/main/java/com/umc/gusto/domain/myCategory/service/MyCategoryServiceImpl.java
+++ gusto/src/main/java/com/umc/gusto/domain/myCategory/service/MyCategoryServiceImpl.java
@@ -20,7 +20,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -65,26 +64,6 @@ public class MyCategoryServiceImpl implements MyCategoryService {
 
     }
 
-//    @Transactional(readOnly = true)
-//    public List<MyCategoryResponse> getAllMyCategoryWithLocation(User user, String townName) {
-//        List<MyCategory> myCategoryList = myCategoryRepository.findByStatusAndPublishCategoryAndUser(user);                                      // 특정 townName인 storesList
-//
-//        return myCategoryList.stream()
-//                .map(myCategory -> {
-//                    List<Pin> pinList = pinRepository.findAllByUserAndMyCategoryOrderByPinIdDesc(myCategory, townName);     // 먼저 카테고리로 구분
-//
-//                    return MyCategoryResponse.builder()
-//                            .myCategoryId(myCategory.getMyCategoryId())
-//                            .myCategoryName(myCategory.getMyCategoryName())
-//                            .myCategoryScript(myCategory.getMyCategoryScript())
-//                            .myCategoryIcon(myCategory.getMyCategoryIcon())
-//                            .publishCategory(user.getPublishCategory())
-//                            .pinCnt(pinList.size())            // pin 개수 받아오기로 변경
-//                            .build();
-//                })
-//                .collect(Collectors.toList());
-//    }
-
     @Transactional(readOnly = true)
     public List<PinByMyCategoryResponse> getAllPinByMyCategory(User user, String nickname, Long myCategoryId, String townName) {
         List<Pin> pinList;
@@ -122,33 +101,6 @@ public class MyCategoryServiceImpl implements MyCategoryService {
                 })
                 .collect(Collectors.toList());
     }
-
-//    @Transactional(readOnly = true)
-//    public List<PinByMyCategoryResponse> getAllPinByMyCategoryWithLocation(User user, Long myCategoryId, String townName) {
-//        List<Pin> pinList = pinRepository.findPinsByUserAndMyCategoryIdAndTownNameAndPinIdDESC(user, myCategoryId, townName);
-//
-//        return getPinByMyCategoryResponses(user, pinList);
-//    }
-//
-//    private List<PinByMyCategoryResponse> getPinByMyCategoryResponses(User user, List<Pin> pinList) {
-//        return pinList.stream()                                     // townName을 기준으로 보일 수 있는 store가 포함된 pin만 보이기
-//                .map(pin -> {
-//                    Store store = pin.getStore();
-//                    Optional<Review> topReviewOptional = reviewRepository.findFirstByStoreOrderByLikedDesc(store);       // 가장 좋아요가 많은 review
-//                    String reviewImg = topReviewOptional.map(Review::getImg1).orElse(null);                               // 가장 좋아요가 많은 review 이미지
-//                    Integer reviewCnt = reviewRepository.countByStoreAndUserNickname(store, user.getNickname());                        // 내가 작성한 리뷰의 개수 == 방문 횟수
-//
-//                    return  PinByMyCategoryResponse.builder()
-//                            .pinId(pin.getPinId())
-//                            .storeId(store.getStoreId())
-//                            .storeName(store.getStoreName())
-//                            .address(store.getAddress())
-//                            .reviewImg(reviewImg)
-//                            .reviewCnt(reviewCnt)
-//                            .build();
-//                })
-//                .collect(Collectors.toList());
-//    }
 
     @Transactional
     public void createMyCategory(User user, CreateMyCategoryRequest createMyCategory) {
