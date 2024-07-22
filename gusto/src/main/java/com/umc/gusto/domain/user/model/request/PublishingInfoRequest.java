70ad7f543dc94e28f41b0651be37b76b7ea4def1
zzo3ozz <wityoung.h@gmail.com>
2024-07-23 00:41:28 +0900

    Revert "fix: publishingInfo Request body에서 publishCategory 매핑을 못하는 문제 수정"
    This reverts commit 12c1516fa240c86604fc1c97ed43d190030de70e.

--- gusto/src/main/java/com/umc/gusto/domain/user/model/request/PublishingInfoRequest.java
+++ gusto/src/main/java/com/umc/gusto/domain/user/model/request/PublishingInfoRequest.java
@@ -1,14 +1,7 @@
 package com.umc.gusto.domain.user.model.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class PublishingInfoRequest {
     private boolean publishReview;
-    @JsonProperty("publishCategory")
     private boolean publishPin;
     private boolean publishRoute;
 
